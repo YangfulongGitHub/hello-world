c51981a6961bc999a76c7d7607da67fa3de3756e
yangfulong_glb <yang@.com>
2019-04-12 15:31:36 +0800

    first commit

---
+++ src/main/java/cn/com/yang/User.java
@@ -0,0 +1,14 @@
+package cn.com.yang;
+
+public class User {
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
